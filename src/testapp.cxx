655e0cfa426d4a780b883aaed03efcf687f45dce
root <root@ibm-p8-kvm-03-guest-02.virt.pnr.lab.eng.rdu2.redhat.com>
2018-02-26 11:36:21 -0500

    First release

---
+++ src/testapp.cxx
@@ -0,0 +1,14 @@
+
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+  while (true) {
+     printf("OpenShift C++ Sample App running, Sleeping for a minute\n");
+     usleep(1000  * 1000 * 60);
+  }
+  return 0;
+}
